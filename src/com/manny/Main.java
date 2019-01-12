e7f9b92c1f26c8bc7c50b571ce8c96aec0f3f152
Manny Aguilar <mannyaaguilar@yahoo.com>
2019-01-11 21:14:10 -0700

    first commit

---
+++ src/com/manny/Main.java
@@ -0,0 +1,26 @@
+package com.manny;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// Challenge!
+        /* Create a simple banking app. There should be a Bank class. It should have an ArrayList of branches.
+        Each branch should have an ArrayList of Customers. The Customers should have an ArrayList of Doubles (transactions)
+        Customer:
+        Name, and ArrayList of doubles.
+        Branch:
+        Need to be able to add a new customer and initial transaction amount. Also needs to add additional transactions
+        for that customer/branch.
+        Bank:
+        Add a new branch
+        Add a customer to that branch with initial transaction
+        Add a transaction for an existing customer for that branch
+        Show a list of customers for a particular branch and optionally a list of their transactions.
+        Demonstrate Autoboxing and Unboxing in your code.
+        hint: Transactions
+        Add data validation.
+        e.g. check if it exists, or does not exist, etc.
+        Think about where you are adding the code to perform certain actions.
+        */
+    }
+}
