006e49c93a57c7d61f8705a0a2e01304768a4fcc
Manny Aguilar <mannyaaguilar@yahoo.com>
2019-01-15 01:20:11 -0700

    created Bank class and functionality

--- src/com/manny/Main.java
+++ src/com/manny/Main.java
@@ -22,5 +22,45 @@ public class Main {
         e.g. check if it exists, or does not exist, etc.
         Think about where you are adding the code to perform certain actions.
         */
+
+        Bank bank = new Bank("US Bank");
+
+        if(bank.addBranch("Avondale")){
+            System.out.println("Avondale branch created");
+        }
+
+
+        bank.addCustomer("Avondale", "Manny", 100000.54);
+        bank.addCustomer("Avondale", "Estella", 100000.55);
+        bank.addCustomer("Avondale", "Andrew", 100000.56);
+
+        bank.addBranch("Glendale");
+        bank.addCustomer("Glendale", "Maria", 100000.57);
+
+        bank.addCustomerTransaction("Avondale", "Manny", 50000.00);
+        bank.addCustomerTransaction("Avondale", "Estella", 25000.00);
+        bank.addCustomerTransaction("Avondale", "Andrew", 75000.00);
+
+        bank.listCustomers("Avondale", true);
+        bank.listCustomers("Glendale", true);
+
+        bank.addBranch("Phoenix");
+
+        if(!bank.addCustomer("Phoenix", "Amber", 5.53)){
+            System.out.println("Error: Phoenix branch does not exist");
+        }
+
+        if(!bank.addBranch("Avondale")){
+            System.out.println("Avondale branch already exists");
+        }
+
+        if(!bank.addCustomerTransaction("Avondale", "Abel", 45.23)){
+            System.out.println("Customer does not exist at branch");
+        }
+
+        if (!bank.addCustomer("Avondale", "Manny", 12.21)) {
+            System.out.println("Customer Manny already exists");
+        }
     }
 }
+
